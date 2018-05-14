8f9b674bf7bfb14a2c3e2a4e90596d86bee00ba0
maqiang123 <936273998@qq.com>
2018-05-15 00:18:21 +0800

    122

--- abc/src/com/ddd.java
+++ abc/src/com/ddd.java
@@ -8,12 +8,13 @@
  */
  class ddd {
 public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("1234");
 	System.out.println("123ddd");
 	System.out.println("1255555553");
-=======
 	System.out.println("123456");
->>>>>>> 4e66706121d061eb652d2ea91656cd5f2f1bbc05
+	System.out.println("123456");
+	System.out.println("123456");
+	System.out.println("123456");
+	System.out.println("123456");
 }
 }
