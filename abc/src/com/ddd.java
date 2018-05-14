1d3d6a9717fd65fc4d5d020ed8975fbfcbac5c29
maqiang123 <936273998@qq.com>
2018-05-14 23:51:28 +0800

    123

--- abc/src/com/ddd.java
+++ abc/src/com/ddd.java
@@ -4,10 +4,10 @@ package com;
  *<p>Title:ddd.java </p>  
  *<p>Description: </p>  
  * @author horse
- * @date 2018年5月14日  
+ * @date 2018骞�5鏈�14鏃�  
  */
-public class ddd {
+ class ddd {
 public static void main(String[] args) {
-	
+	System.out.println(53);
 }
 }
