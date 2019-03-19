db014d7125bd6c4a507cb79c48c76eb05cb2a42c
maqiang <ma_qiang@hoperun.com>
2019-03-19 17:42:18 +0800

    commit代码

--- abc/src/com/ddd.java
+++ abc/src/com/ddd.java
@@ -14,9 +14,9 @@ public static void main(String[] args) {
 	System.out.println("123ddd");
 	System.out.println("11112222333");
 	System.out.println("123456");
-	System.out.println("123456");
-	System.out.println("3232323");
-	System.out.println("123456");
-	System.out.println("123456");
+	for (int i = 0; i < 3; i++) {
+		System.out.println("我是帅哥！");
+	}
+	System.out.println("dfsafdsafds");
 }
 }
